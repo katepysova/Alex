7f3f4ca8ea41c8a5972c1018d81b99a759a27944
Kateryna <katepysova97@gmail.com>
2022-12-06 16:50:15 +0100

    Added fullpage.js

---
+++ src/js/constants.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/prefer-default-export
+export const mediaQueries = {
+  breakpointExtraLarge: 1350,
+  breakpointLarge: 1200,
+  breakPointMedium: 990,
+  breakPointSmall: 750,
+  breakPointExtraSmall: 450,
+};
--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,9 @@
+import Fullpage from "fullpage.js";
+import { mediaQueries } from "./constants.js";
+
 const headerEl = document.querySelector(".header");
 const menuTogglerBtn = document.querySelector(".header__nav-toggler");
-const html = document.querySelector("html");
+const html = document.querySelector("main");
 
 menuTogglerBtn.addEventListener("click", () => {
   headerEl.classList.toggle("active");
@@ -15,4 +18,19 @@ window.addEventListener("resize", () => {
   if (html.classList.remove("u-static")) {
     html.classList.remove("u-static");
   }
+});
+
+// eslint-disable-next-line no-unused-vars
+const _ = new Fullpage("#fullpage", {
+  licenseKey: process.env.FULLPAGE_JS_KEY || "",
+  responsiveWidth: mediaQueries.breakpointExtraLarge,
+  autoScrolling: true,
+  fadingEffect: true,
+  css3: true,
+  easingcss3: "ease-in",
+  fitToSection: true,
+  scrollOverflow: false,
+  scrollBar: true,
+  keyboardScrolling: true,
+  recordHistory: false,
 });
